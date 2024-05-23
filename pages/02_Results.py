b81eda51648660c3ba036c41b7de535cb55d5eb0
beacmreyes <beacmreyes@gmail.com>
2024-05-23 19:02:15 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("Results")
 st.write("Here you can put each of your key results.")
 
-st.image("../images/total_spending.png")
+st.image("./images/total_spending.png")
 st.write("=Spending per categoory...")
